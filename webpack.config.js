28c0d1cec97a172ad5a81572952b696e323bbf6a
Ashton Six <me@ashtonsix.com>
2018-01-19 11:18:51 +0000

    Add webpack/prettier/eslint/package-lock

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'render-prop.js',
+    library: 'RenderProp',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react'],
+            plugins: ['transform-class-properties']
+          }
+        }
+      }
+    ]
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React'
+    }
+  }
+}
