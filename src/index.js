b1fcf6b9268374869642d7a8b077ed8f5575693d
Ashton Six <me@ashtonsix.com>
2018-01-20 20:21:21 +0000

    Remove `handleActions` utility (superflous API)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import RenderProp from './RenderProp'
 import Store from './Store'
-import handleActions from './handleActions'
 import compare from './compare'
 
 export default RenderProp
-export {RenderProp, Store, handleActions, compare}
+export {RenderProp, Store, compare}
